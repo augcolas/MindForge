e0df4545dc2305e95202692708d35a2019c05c0c
augcolas <augus.colas@orange.fr>
2023-11-29 09:59:15 +0100

    feat : home style

--- App.tsx
+++ App.tsx
@@ -3,14 +3,5 @@ import {Button, StyleSheet, Text, View} from 'react-native';
 import Home from "./views/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Home/>
-    </View>
-  );
+  return (<Home/>);
 }
-
-const styles = StyleSheet.create({
-  container: {
-  },
-});
--- views/Home.tsx
+++ views/Home.tsx
@@ -1,9 +1,23 @@
-import styles from  '../styles/Home.tsx'
+
+import { StyleSheet, View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Home(){
-		return (
-			<div style={styles.container}>
-				<h1>Home</h1>
-			</div>
-		)
+	return (
+		<LinearGradient
+			colors={['rgba(27,109,22,1)', 'rgba(23,52,18,1)']}
+			style={styles.background}
+		>
+			<Text>Open up App.tsx to start working on your app!</Text>
+		</LinearGradient>
+	);
 }
+
+const styles = StyleSheet.create({
+	background: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+	}
+});
