c8f4a32d4ec394156ee077fc427b665e78c93a3d
augcolas <augus.colas@orange.fr>
2023-11-29 11:16:20 +0100

    feat : add navigation

--- front/App.tsx
+++ front/App.tsx
@@ -1,7 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Home from "./views/Home";
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return (<Home/>);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        />
+        <Stack.Screen
+          name="Game"
+          component={Home}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
